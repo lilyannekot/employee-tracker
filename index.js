a0fd19b65c1a1cd6d9b2895beca74e0ae09f2786
lilys tacos <lilyannek@gmail.com>
2022-07-28 16:44:07 -0700

    variables for questions, adding responses to database

--- index.js
+++ index.js
@@ -16,80 +16,156 @@ const db = mysql.createConnection(
 
 // Questions to add new department
 const addDepartment = [
-    {
-        type: "input",
-        name: "departmentName",
-        message: "What is the name of the department you'd like to add?",
-    }
+  {
+    type: "input",
+    name: "departmentName",
+    message: "What is the name of the department you'd like to add?",
+  },
 ];
 
+// Questions to add new role
 const addRole = [
-    {
-        type: "input",
-        name: "roleName",
-        message: "What is the name of the role you'd like to add?"
-    },
-    {
-        type: "input",
-        name: "roleSalary",
-        message: "What is the salary of the role you'd like to add?"
-    },
-    {
-        type: "list",
-        name: "roleDepartment",
-        message: "What department does the role belong in?",
-        choices: [
-            "Engineering",
-            "Marketing",
-            "Sales",
-            "Finance",
-            "Legal",
-        ],
-    },
+  {
+    type: "input",
+    name: "roleTitle",
+    message: "What is the name of the role you'd like to add?",
+  },
+  {
+    type: "input",
+    name: "roleSalary",
+    message: "What is the salary of the role you'd like to add?",
+  },
+  {
+    type: "list",
+    name: "roleDepartment",
+    message: "What department does the role belong in?",
+    choices: ["Engineering", "Marketing", "Sales", "Finance", "Legal"],
+  },
+];
+
+// Questions to add new employee
+const addEmployee = [
+  {
+    type: "input",
+    name: "employeeFirstName",
+    message: "What is the first name of the employee you'd like to add?",
+  },
+  {
+    type: "input",
+    name: "employeeLastName",
+    message: "What is the last name of this employee?",
+  },
+  {
+    type: "input",
+    name: "employeeRole",
+    message: "What is the role of this employee?",
+  },
+  {
+    type: "list",
+    name: "employeeManager",
+    message: "Who is the employee's manager?",
+    choices: [
+      "Greg Marzec, CFO",
+      "Sally Mae, Software Engineer 1",
+      "John Smith, Senior Software Engineer",
+      "Josh Scheuring, SDR",
+      "Jamie Roe, Sales AE",
+      "Alex Farley, Lawyer",
+      "Nicholas Bergo, Legal Assistant",
+      "Lily Kot, Director of Marketing",
+      "Kelli Frank, Demand Generation Lead",
+      "Bryce Watson, Accountant",
+    ],
+  },
+];
+
+// Questions to update an employee
+const updateEmployee = [
+  {
+    type: "list",
+    name: "selectEmployee",
+    message: "What is the role of this employee??",
+    choices: [
+      "Greg Marzec",
+      "Sally Mae",
+      "John Smith",
+      "Josh Scheuring",
+      "Jamie Roe",
+      "Alex Farley",
+      "Nicholas Bergo",
+      "Lily Kot",
+      "Kelli Frank",
+      "Bryce Watson",
+    ],
+  },
 ];
 
 function init() {
-inquirer
-  .prompt([
-    {
-      type: "list",
-      name: "starterPrompt",
-      message: "What would you like to do?",
-      choices: [
-        "View all departments",
-        "View all roles",
-        "View all employees",
-        "Add a department",
-        "Add a role",
-        "Add an employee",
-        "Update an employee role",
-      ],
-    },
-  ])
-  .then(function(answers) {
-    if (answers.starterPrompt === "View all departments") {
-      db.query("SELECT * FROM department", (err, results) => {
-        console.log(results);
-      });
-    } else if (answers.starterPrompt === "View all roles") {
-      db.query("SELECT * FROM role", (err, results) => {
-        console.log(results);
-      });
-    } else if (answers.starterPrompt === "View all employees") {
-      db.query("SELECT * FROM employee", (err, results) => {
-        console.log(results);
-      });
-    } else if (answers.starterPrompt === "Add a department") {
-        inquirer.prompt(addDepartment)
-        .then (function(data) {
-            console.log(data);
-            // How do I get the new department added to the list of all departments?
-        })
-    } else if (answers.starterPrompt === "Add a role") {
-        inquirer.prompt(addRole)
-        .then (function(data)) {
-            console.log(data);
-        }
-    }
-  })
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "starterPrompt",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee role",
+        ],
+      },
+    ])
+    .then(function (answers) {
+      console.log(answers);
+      if (answers.starterPrompt === "View all departments") {
+        db.query("SELECT * FROM department", (err, results) => {
+          console.table(results);
+          return init();
+        });
+      } else if (answers.starterPrompt === "View all roles") {
+        db.query("SELECT * FROM role", (err, results) => {
+          console.table(results);
+          return init();
+        });
+      } else if (answers.starterPrompt === "View all employees") {
+        db.query("SELECT * FROM employee", (err, results) => {
+          console.table(results);
+          return init();
+        });
+      } else if (answers.starterPrompt === "Add a department") {
+        inquirer.prompt(addDepartment).then(function (data) {
+          console.table(data);
+          db.query(`INSERT INTO department VALUES("${data.departmentName}")`);
+          // How do I get the new department added to the list of all departments?
+          return init();
+        });
+      } else if (answers.starterPrompt === "Add a role") {
+        inquirer.prompt(addRole).then(function (data) {
+          console.table(data);
+          // What do I change departmentName to to have all the info added to the role table?
+          db.query(
+            `INSERT INTO role (title, department, salary) VALUES("${data.roleTitle}", "${data.roleDepartment}", "${data.roleSalary}")`
+          );
+
+          return init();
+        });
+      } else if (answers.starterPrompt === "Add an employee") {
+        inquirer.prompt(addEmployee).then(function (data) {
+          console.table(data);
+
+          return init();
+        });
+      } else if (answers.starterPrompt === "Update an employee role") {
+        inquirer.prompt(updateEmployee).then(function (data) {
+          console.table(data);
+
+          return init();
+        });
+      }
+    });
 }
+
+init();
