acf57af15fe39d0837196d97f3ea4a01ee8982b2
lilys tacos <lilyannek@gmail.com>
2022-07-28 15:58:11 -0700

    troubleshooting module not found

--- index.js
+++ index.js
@@ -14,6 +14,41 @@ const db = mysql.createConnection(
   console.log(`Connected to the employee_db database!`)
 );
 
+// Questions to add new department
+const addDepartment = [
+    {
+        type: "input",
+        name: "departmentName",
+        message: "What is the name of the department you'd like to add?",
+    }
+];
+
+const addRole = [
+    {
+        type: "input",
+        name: "roleName",
+        message: "What is the name of the role you'd like to add?"
+    },
+    {
+        type: "input",
+        name: "roleSalary",
+        message: "What is the salary of the role you'd like to add?"
+    },
+    {
+        type: "list",
+        name: "roleDepartment",
+        message: "What department does the role belong in?",
+        choices: [
+            "Engineering",
+            "Marketing",
+            "Sales",
+            "Finance",
+            "Legal",
+        ],
+    },
+];
+
+function init() {
 inquirer
   .prompt([
     {
@@ -25,12 +60,13 @@ inquirer
         "View all roles",
         "View all employees",
         "Add a department",
+        "Add a role",
         "Add an employee",
         "Update an employee role",
       ],
     },
   ])
-  .then(function (answers) {
+  .then(function(answers) {
     if (answers.starterPrompt === "View all departments") {
       db.query("SELECT * FROM department", (err, results) => {
         console.log(results);
@@ -43,5 +79,17 @@ inquirer
       db.query("SELECT * FROM employee", (err, results) => {
         console.log(results);
       });
+    } else if (answers.starterPrompt === "Add a department") {
+        inquirer.prompt(addDepartment)
+        .then (function(data) {
+            console.log(data);
+            // How do I get the new department added to the list of all departments?
+        })
+    } else if (answers.starterPrompt === "Add a role") {
+        inquirer.prompt(addRole)
+        .then (function(data)) {
+            console.log(data);
+        }
     }
-  });
+  })
+}
